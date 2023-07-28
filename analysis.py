1ddcbb04a1ee2a49c69964b2dfaeef386d5d8b46
Desmond <desmondfowler14@gmail.com>
2023-07-28 15:35:14 -0500

    Playing with twint

--- analysis.py
+++ analysis.py
@@ -4,19 +4,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import time
 
-# Depth of recursion
-MAX_DEPTH = 3
-
 # Twint requires these apparently:
 c = twint.Config()
 c.Utc = True
 c.Full_text = True
 c.Hide_output = True
 c.Store_object = True
-c.Limit = 250
 
 # Get Elon Musk's tweets
-c.Username = "elonmusk"
+c.Username = input("Which username would you like to center the network on? ")
+c.Limit = int(input("How many recent tweets do you want to look at? "))
+depth = int(input("What depth do you want to use? (Default 0) "))
+
 twint.run.Search(c)
 tweets = twint.output.tweets_list
 
@@ -24,12 +23,10 @@ tweets = twint.output.tweets_list
 G = nx.DiGraph()
 
 
-def add_replies(tweet, depth=0):
+def add_replies(tweet, depth=0, max_id=None):
     """
     Recursively add replies to the graph.
     """
-    if depth > MAX_DEPTH:
-        return
     reply_to = tweet.reply_to
     if reply_to:
         if isinstance(reply_to, dict):
@@ -42,19 +39,21 @@ def add_replies(tweet, depth=0):
         c.Utc = True
         c.Full_text = True
         c.Username = None
-        c.Search = "from:" + tweet.username + " conversation_id:" + reply_to_id
-        c.Hide_output = True
-        c.Store_object = True
-        c.Store_object_tweets_list = True
+        if max_id is None:
+            c.Since_id = reply_to_id
+        else:
+            c.Since_id = None
+            c.Max_id = max_id
         twint.run.Search(c)
         time.sleep(10)
         reply_to_tweet = twint.output.tweets_list[-1]
-        add_replies(reply_to_tweet, depth=depth+1)
+        add_replies(reply_to_tweet, depth=depth+1, max_id=reply_to_id)
 
 
 # Add Elon Musk's tweets and their replies
 for tweet in tweets:
-    add_replies(tweet)
+    add_replies(tweet, depth)
+    time.sleep(5)  # Wait for 5 seconds between requests
 
 # Write the graph to a CSV file
 nx.write_edgelist(G, "twitter_social_network.csv", delimiter=",", data=False)
